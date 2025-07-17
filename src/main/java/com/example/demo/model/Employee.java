8cb4f921277a6f5e3c78985b6f0c4447f6e77bfe
Kayalvizhi Shanmugam <kayalshanmugam.techsavvy@gmail.com>
2025-07-18 03:03:11 +0530

    Update Employee.java

--- src/main/java/com/example/demo/model/Employee.java
+++ src/main/java/com/example/demo/model/Employee.java
@@ -2,7 +2,7 @@ package com.example.demo.model;
 
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
-
+import lombok.Data;
 @Document(collection = "employees")
 @Data
 public class Employee {
